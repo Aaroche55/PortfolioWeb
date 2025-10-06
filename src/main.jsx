cc84424968203142fb19bfd909530c69b7f00dd3
aaroche55 <aaroche55@gmail.com>
2025-10-06 15:27:09 +0100

    router fix

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/PortfolioWeb">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
